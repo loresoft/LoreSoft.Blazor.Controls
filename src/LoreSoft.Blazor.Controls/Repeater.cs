f842065bb4f072f132cbef08d403c2831a550da5
Paul Welter <pwelter@loresoft.com>
2024-04-12 08:41:18 -0500

    update Repeater

--- src/LoreSoft.Blazor.Controls/Repeater.cs
+++ src/LoreSoft.Blazor.Controls/Repeater.cs
@@ -1,23 +1,25 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿#nullable enable
+
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 
 namespace LoreSoft.Blazor.Controls;
 
 public class Repeater<TItem> : ComponentBase
 {
-    [Parameter, EditorRequired]
-    public IEnumerable<TItem> Items { get; set; }
-
-    [Parameter, EditorRequired]
-    public RenderFragment<TItem> ChildContent { get; set; }
+    [Parameter]
+    public IEnumerable<TItem>? Items { get; set; }
 
     [Parameter]
-    public RenderFragment Empty { get; set; }
+    public RenderFragment<TItem>? ChildContent { get; set; }
+
+    [Parameter]
+    public RenderFragment? Empty { get; set; }
 
     /// <inheritdoc />
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        if (Items?.Any() == true)
+        if (ChildContent != null && Items?.Any() == true)
         {
             foreach (var item in Items)
             {
