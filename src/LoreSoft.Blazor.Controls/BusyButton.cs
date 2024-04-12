f6a9948371a202f17b797d53efd3d1a3984ebcd6
Paul Welter <pwelter@loresoft.com>
2024-04-11 23:36:23 -0500

    improve BusyButton, Add StateProvider, add ExpandTracker

--- src/LoreSoft.Blazor.Controls/BusyButton.cs
+++ src/LoreSoft.Blazor.Controls/BusyButton.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace LoreSoft.Blazor.Controls;
 
@@ -12,6 +13,9 @@ public class BusyButton : ComponentBase
     public bool Disabled { get; set; }
 
     [Parameter]
+    public string BusyText { get; set; } = "Processing";
+
+    [Parameter]
     public RenderFragment BusyTemplate { get; set; }
 
     [Parameter]
@@ -20,30 +24,80 @@ public class BusyButton : ComponentBase
     [Parameter(CaptureUnmatchedValues = true)]
     public Dictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
 
+    [Parameter]
+    public EventCallback Trigger { get; set; }
+
+    private bool Executing { get; set; }
+
     /// <inheritdoc />
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
         builder.OpenElement(0, "button");
         builder.AddMultipleAttributes(1, Attributes);
-        builder.AddAttribute(2, "disabled", Disabled || Busy);
+        builder.AddAttribute(2, "disabled", Disabled || IsBusy);
 
-        if (Busy)
+        if (Trigger.HasDelegate)
         {
-            builder.AddContent(3, BusyTemplate);
+            builder.AddAttribute(3, "type", "button");
+            builder.AddAttribute(4, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, ExecuteTrigger));
+        }
+
+        if (IsBusy)
+        {
+            builder.AddContent(5, BusyTemplate);
         }
         else
         {
-            builder.AddContent(3, ChildContent);
+            builder.AddContent(6, ChildContent);
         }
 
         builder.CloseElement(); // button
     }
+
 
     /// <inheritdoc />
     protected override void OnParametersSet()
     {
         base.OnParametersSet();
 
-        BusyTemplate ??= builder => builder.AddContent(0, "Busy...");
+        BusyTemplate ??= builder => {
+            builder.AddContent(0, BusyText);
+
+            builder.OpenElement(1, "span");
+            builder.AddAttribute(2, "aria-hidden", "true");
+            builder.AddAttribute(3, "class", "busy-loading-indicator");
+
+            builder.OpenElement(4, "span");
+            builder.AddAttribute(5, "class", "busy-loading-dot-1");
+            builder.CloseElement();
+
+            builder.OpenElement(6, "span");
+            builder.AddAttribute(7, "class", "busy-loading-dot-2");
+            builder.CloseElement();
+
+            builder.OpenElement(8, "span");
+            builder.AddAttribute(9, "class", "busy-loading-dot-3");
+            builder.CloseElement();
+
+            builder.CloseElement(); //span
+        };
+    }
+
+    protected bool IsBusy => Busy || Executing;
+
+    private async Task ExecuteTrigger()
+    {
+        if (!Trigger.HasDelegate)
+            return;
+
+        try
+        {
+            Executing = true;
+            await Trigger.InvokeAsync();
+        }
+        finally
+        {
+            Executing = false;
+        }
     }
 }
---
+++ src/LoreSoft.Blazor.Controls/Utilities/ExpandTracker.cs
@@ -0,0 +1,34 @@
+﻿namespace LoreSoft.Blazor.Controls.Utilities;
+
+public class ExpandTracker<TItem>
+{
+    private readonly HashSet<TItem> _expandedItems = [];
+
+    public event Action OnChange;
+
+    public bool IsExpanded(TItem item)
+    {
+        return _expandedItems.Contains(item);
+    }
+
+    public void Toggle(TItem item)
+    {
+        if (_expandedItems.Contains(item))
+            _expandedItems.Remove(item);
+        else
+            _expandedItems.Add(item);
+
+        NotifyStateChanged();
+    }
+
+    public virtual void Clear()
+    {
+        _expandedItems.Clear();
+        NotifyStateChanged();
+    }
+
+    public void NotifyStateChanged()
+    {
+        OnChange?.Invoke();
+    }
+}
